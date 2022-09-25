488200c4b176cc61dc7e1447f07ce1cb3786765d
KingSmoke-cyber <114359472+KingSmoke-cyber@users.noreply.github.com>
2022-09-25 21:00:41 +0200

    Create Binary.py

---
+++ Binary.py
@@ -0,0 +1,14 @@
+print("Enter the binary number: " , end= "")
+bnum = int(input())
+
+dnum = 0
+m = 1
+blen = len(str(bmum))
+
+for k in range(blen):
+      rem = bnum%10
+      dnum = dnum + ( rem * m)
+      m = m * 2
+      bnum = int(bnum/10)
+      
+print("\nEquivalent Decimal Value = " , dnum)
